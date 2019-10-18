fe9ce4e7c87d2ebe9089492e54d1c5a26b025046
安宁 <38878763+peacetrue@users.noreply.github.com>
2019-10-18 16:16:16 +0800

    Initial commit

---
+++ peacetrue-demo-service-mybatis/src/main/java/com/github/peacetrue/demo/service/Demo.java
@@ -0,0 +1,27 @@
+package com.github.peacetrue.demo.service;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @author xiayx
+ */
+@Getter
+@Setter
+@ToString
+public class Demo<Id, OperatorId> implements Serializable {
+
+    private static final long serialVersionUID = 0L;
+
+    private Id id;
+    private String code;
+    private String name;
+    private OperatorId creatorId;
+    private Date createdTime;
+    private OperatorId modifierId;
+    private Date modifiedTime;
+}
